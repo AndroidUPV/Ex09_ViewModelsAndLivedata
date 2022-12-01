044d0f56d6e051ab2543d74e9dfa75a69e3b282d
ddandres <ddandres@disca.upv.es>
2022-12-01 17:11:03 +0100

    Initial commit

---
+++ app/src/main/java/upv/dadm/ex09_viewmodelsandlivedata/ui/activities/MainActivity.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2022
+ * David de Andrés and Juan Carlos Ruiz
+ * Development of apps for mobile devices
+ * Universitat Politècnica de València
+ */
+
+package upv.dadm.ex09_viewmodelsandlivedata.ui.activities
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import upv.dadm.ex09_viewmodelsandlivedata.databinding.ActivityMainBinding
+import upv.dadm.ex09_viewmodelsandlivedata.ui.viewmodels.LikeViewModel
+
+/**
+ * Displays the number of likes, which can be increased/decreased using the provided Buttons.
+ */
+class MainActivity : AppCompatActivity() {
+
+    // Get the ViewModel from the default Factory
+    private val viewModel: LikeViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Get the automatically generated view binding for the layout resource
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        // Set the activity content to the root element of the generated view
+        setContentView(binding.root)
+
+        // Observe the number of likes and display it on a TextView
+        viewModel.like.observe(this@MainActivity) { like ->
+            binding.tvLike.text = like
+        }
+        // Increase the current number of likes
+        binding.bThumbUp.setOnClickListener { viewModel.thumbUp() }
+        // Decrease the current number of likes
+        binding.bThumbDown.setOnClickListener { viewModel.thumbDown() }
+    }
+}
---
+++ app/src/main/java/upv/dadm/ex09_viewmodelsandlivedata/ui/viewmodels/LikeViewModel.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2022
+ * David de Andrés and Juan Carlos Ruiz
+ * Development of apps for mobile devices
+ * Universitat Politècnica de València
+ */
+
+package upv.dadm.ex09_viewmodelsandlivedata.ui.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+
+/**
+ * Holds the number of likes received and provides methods for increasing/decreasing it.
+ */
+class LikeViewModel : ViewModel() {
+
+    // UI state (immutable): number of likes
+    private val _like: MutableLiveData<Int> = MutableLiveData(0)
+    // Backing property (mutable)
+    val like: LiveData<String>
+        // Int is transformed into String to be easily displayed by the UI
+        get() = Transformations.map(_like) { value -> value.toString() }
+
+    /**
+     * Increases the current number of likes.
+     */
+    fun thumbUp() {
+        _like.value = _like.value?.plus(1)
+    }
+
+    /**
+     * Decreases the current number of likes (it can be negative!).
+     */
+    fun thumbDown() {
+        _like.value = _like.value?.minus(1)
+    }
+}
