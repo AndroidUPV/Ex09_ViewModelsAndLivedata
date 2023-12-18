04acf0d1515461a26ee9060766ded4c28357de4f
ddandres <ddandres@disca.upv.es>
2023-12-18 16:10:30 +0100

    Theme updated to Material3 Build configuration updated from Groovy to Kotlin

--- app/src/main/java/upv/dadm/ex09_viewmodelsandlivedata/ui/viewmodels/LikeViewModel.kt
+++ app/src/main/java/upv/dadm/ex09_viewmodelsandlivedata/ui/viewmodels/LikeViewModel.kt
@@ -1,16 +1,20 @@
 /*
- * Copyright (c) 2022
- * David de Andrés and Juan Carlos Ruiz
- * Development of apps for mobile devices
- * Universitat Politècnica de València
+ * Copyright (c) 2022-2023 Universitat Politècnica de València
+ * Authors: David de Andrés and Juan Carlos Ruiz
+ *          Fault-Tolerant Systems
+ *          Instituto ITACA
+ *          Universitat Politècnica de València
+ *
+ * Distributed under MIT license
+ * (See accompanying file LICENSE.txt)
  */
 
 package upv.dadm.ex09_viewmodelsandlivedata.ui.viewmodels
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
 
 /**
  * Holds the number of likes received and provides methods for increasing/decreasing it.
@@ -19,10 +23,11 @@ class LikeViewModel : ViewModel() {
 
     // UI state (mutable): number of likes
     private val _like: MutableLiveData<Int> = MutableLiveData(0)
+
     // Backing property (immutable)
     val like: LiveData<String>
         // Int is transformed into String to be easily displayed by the UI
-        get() = Transformations.map(_like) { value -> value.toString() }
+        get() = _like.map { value -> value.toString() }
 
     /**
      * Increases the current number of likes.
