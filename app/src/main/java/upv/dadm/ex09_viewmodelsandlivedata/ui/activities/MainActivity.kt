f798b1114f5c6a4ef67ef591b589062ffc3d6736
ddandres <ddandres@disca.upv.es>
2024-12-19 15:35:47 +0100

    Updated to AGP 8.7.3 and gradle version 8.9 Updated to edge-to-edge display

--- app/src/main/java/upv/dadm/ex09_viewmodelsandlivedata/ui/activities/MainActivity.kt
+++ app/src/main/java/upv/dadm/ex09_viewmodelsandlivedata/ui/activities/MainActivity.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022-2023 Universitat Politècnica de València
+ * Copyright (c) 2022-2024 Universitat Politècnica de València
  * Authors: David de Andrés and Juan Carlos Ruiz
  *          Fault-Tolerant Systems
  *          Instituto ITACA
@@ -12,8 +12,11 @@
 package upv.dadm.ex09_viewmodelsandlivedata.ui.activities
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import upv.dadm.ex09_viewmodelsandlivedata.databinding.ActivityMainBinding
 import upv.dadm.ex09_viewmodelsandlivedata.ui.viewmodels.LikeViewModel
 
@@ -29,8 +32,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         // Get the automatically generated view binding for the layout resource
         val binding = ActivityMainBinding.inflate(layoutInflater)
+        // Enable edge-to-edge display
+        enableEdgeToEdge()
         // Set the activity content to the root element of the generated view
         setContentView(binding.root)
+        // Prevent the layout from overlapping with system bars in edge-to-edge display
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         // Observe the number of likes and display it on a TextView
         viewModel.like.observe(this@MainActivity) { like ->
